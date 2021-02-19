6ed5c505ca783b3b2fee0dd7dd1fe52e7251e0aa
Raphael Büchi <raphael.buechi@hotmail.com>
2021-02-19 17:47:17 +0100

    Fixed Product Images

--- pages/product_detail/_id.vue
+++ pages/product_detail/_id.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="section">
     <div class="card is-clearfix columns">
-      <figure v-id="product-image" class="card-image is-480x480 column is-one-thirds">
-        <img src="https://bulma.io/images/placeholders/480x480.png" />
+      <figure id="product-image" class="card-image is-480x480 column is-one-thirds">
+        <!-- <img src="https://bulma.io/images/placeholders/480x480.png" /> -->
 
-       <!-- <div v-if="product.title.indexOf('Rot') !== -1">
-          <img src="https://bulma.io/images/placeholders/480x480.png" />
-        </div> -->
-
-        <!-- <div v-if="product.title.indexOf('Grün') !== -1">
-          <img src="../static/images/green.png" />
+       <div v-if="this.$store.getters.getProductById(this.$route.params.id).title.indexOf('Rot') !== -1">
+          <img src="../../static/images/red.png" />
         </div>
 
-        <div v-if="product.title.indexOf('Blau') !== -1">
-          <img src="../static/images/blue.png" />
-        </div> -->
+       <div v-if="this.$store.getters.getProductById(this.$route.params.id).title.indexOf('Grün') !== -1">
+          <img src="../../static/images/green.png" />
+        </div>
+
+        <div v-if="this.$store.getters.getProductById(this.$route.params.id).title.indexOf('Blau') !== -1">
+          <img src="../../static/images/blue.png" />
+        </div>
 
       </figure>
       <div class="card-content column is-two-thirds">
